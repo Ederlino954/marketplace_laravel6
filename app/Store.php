73a14ebb8926e5627ad359a9ecda8bdd62ac4533
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-15 01:36:39 -0300

    30 - Mapeamento 1 para 1 Usuário e Loja

--- app/Store.php
+++ app/Store.php
@@ -6,8 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    public function user() // rel 1:1
+    {
+        return $this->belongsTo(User::class);
+        // return $this->belongsTo(User::class, 'usuario_id-exemplo'); // nomeclatura fora de padrão
+    }
+
 }
+
+
+// 1:1 - Um pra um (Usuário e loja) hasOne e belongsTo
+// 1:N - Um pra Muitos (Loja e Produtos) hasMany e belongsTo
+// N:N - Muitos pra muitos (Produtos e categorias) | belongsToMany
+
 
 // Store -> stores = procura por plural no banco de dados
 // stores
--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', // exemplo'name',
     ];
 
     /**
@@ -33,7 +33,20 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
+    protected $casts = [ // converte um valo com base nas colunas
         'email_verified_at' => 'datetime',
+        // 'name' => 'boolean', // exemplo
     ];
+
+    public function store() // rel 1:1
+    {
+        return $this->hasOne(Store::class);
+        // return $this->hasOne(Store::class, 'usuario_id-exemplo'); // nomeclatura fora de padrão
+    }
 }
+
+
+// 1:1 - Um pra um (Usuário e loja) hasOne e belongsTo
+// 1:N - Um pra Muitos (Loja e Produtos) hasMany e belongsTo
+// N:N - Muitos pra muitos (Produtos e categorias) | belongsToMany
+
