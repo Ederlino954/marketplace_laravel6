0a043ba1800707e8630ef2527775099f23838f94
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-14 23:10:35 -0300

    20 - Model factories

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,15 +12,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // \DB::table('users')->insert(
-        DB::table('users')->insert(
-            [
-                'name' => 'Administrator',
-                'email' => 'admin@admin.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => 'asdfoohadsfashdlkFLGDSA',
-            ]
-        );
+        // DB::table('users')->insert(
+        //     [
+        //         'name' => 'Administrator',
+        //         'email' => 'admin@admin.com',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'remember_token' => 'asdfoohadsfashdlkFLGDSA',
+        //     ]
+        // );
+
+        factory(\App\User::class, 40)->create();
     }
 }
