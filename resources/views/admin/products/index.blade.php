92d85e387a3f008886bacfc66d7a5af2589200c0
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-18 14:22:40 -0300

    55 - Removendo produtos

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-    @section('content') 
+    @section('content')
 
         <a href="{{ route('admin.products.create') }}" class="btn btn-lg btn-success">Criar Produto</a>
 
@@ -18,11 +18,18 @@
                     <tr>
                         <td>{{$p->id}}</td>
                         <td>{{$p->name}}</td>
+                        <td>R$ {{number_format($p->price, 2, ',', '.')}}</td>
                         <td>
-                            <a href="{{ route('admin.products.edit', ['product' => $p->id] )}}" class="btn btn-sm btn-primary">EDITAR</a>
-                            <a href="{{ route('admin.products.destroy', ['product' => $p->id] )}}" class="btn btn-sm btn-danger">REMOVER</a>
-                        </td>                
-                    </tr>            
+                            <div class="btn-group">
+                                <a href="{{ route('admin.products.edit', ['product' => $p->id] )}}" class="btn btn-sm btn-primary">EDITAR</a>
+                                <form action="{{ route('admin.products.destroy', ['product' => $p->id] )}}" method="post" >
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"  class="btn btn-sm btn-danger">REMOVER</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
                 @endforeach
             </tbody>
         </Table>
