2700d71fc1f524e99cc4d044cc047f657c17bec4
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-19 00:03:34 -0300

    61 - Criando views com autenticação

--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-
-    return view('welcome');
-});
-
-
-Route::prefix('admin')->name('admin.')->namespace('admin')->group(function(){
-
-    // Route::prefix('stores')->name('stores.')->group(function(){
-
-    //     Route::get('/', 'StoreController@index')->name('index');
-    //     Route::get('/create', 'StoreController@create')->name('create');
-    //     Route::post('/store', 'StoreController@store')->name('store');
-    //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-    //     Route::post('/update/{store}', 'StoreController@update')->name('update');
-    //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-
-    // });
-
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
-
-});
-
-
 
 
 //Route::get  // pega
@@ -199,3 +174,33 @@ Route::get('/model', function () {
     return $products->categories;
 
 });
+
+
+Route::get('/', function () {
+
+    return view('welcome');
+});
+
+
+Route::prefix('admin')->name('admin.')->namespace('admin')->group(function(){
+
+    // Route::prefix('stores')->name('stores.')->group(function(){
+
+    //     Route::get('/', 'StoreController@index')->name('index');
+    //     Route::get('/create', 'StoreController@create')->name('create');
+    //     Route::post('/store', 'StoreController@store')->name('store');
+    //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+    //     Route::post('/update/{store}', 'StoreController@update')->name('update');
+    //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+
+    // });
+
+    Route::resource('stores', 'StoreController');
+    Route::resource('products', 'ProductController');
+
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
