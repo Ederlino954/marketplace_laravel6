084d8ffcbf7492191bbd27c674a59c76253c5efd
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-20 22:27:21 -0300

    73 - Exibindo validações nas views

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,42 +1,67 @@
 @extends('layouts.app')
 
-@section('content')
+    @section('content')
+        <h1>Criar Loja</h1>
 
-    <h1>Criar Loja</h1>
+        <form action="{{route('admin.stores.store')}}" method="post">
 
-    <form action="{{ route('admin.stores.store') }}" method="post">
-        {{-- @csrf --}}
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <div class="form-group">
+                <label>Nome Loja</label>
+                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
 
-        <div class="form-group">
-            <label>Nome Loja</label>
-            <input type="text" name="name" class="form-control">
-        </div>
+                @error('name')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
 
-        <div  class="form-group">
-            <label>Descrição</label>
-            <input type="text" name="description" class="form-control">
-        </div>
+            </div>
 
-        <div  class="form-group">
-            <label>Telefone</label>
-            <input type="text" name="phone" class="form-control">
-        </div>
+            <div class="form-group">
+                <label>Descrição</label>
+                <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
 
-        <div class="form-group">
-            <label>Celular/Whatsapp</label>
-            <input type="text" name="mobile_phone" class="form-control">
-        </div>
+                @error('description')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
 
-        <div class="form-group">
-            <label>Slug</label>
-            <input type="text" name="slug" class="form-control">
-        </div>
+            </div>
 
-        <div>
-            <button type="submit" class="btn btn-lg btn-success">Criar Loja</button>
-        </div>
+            <div class="form-group">
+                <label>Telefone</label>
+                <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror ">
 
-    </form>
+                @error('phone')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
 
-@endsection
+            </div>
+
+            <div>
+                <label>Celular/Whatsapp</label>
+                <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+
+                @error('mobile_phone')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
+
+            </div>
+
+            <div class="form-group">
+                <label>Slug</label>
+                <input type="text" name="slug" class="form-control">
+            </div>
+
+            <div >
+                <button type="submit" class="btn btn-lg btn-success">Criar Loja</button>
+            </div>
+
+        </form>
+    @endsection
