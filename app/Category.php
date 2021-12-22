f35c81bdfb7bfd33913fe80a093414a027d4c97f
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-21 23:38:55 -0300

    82 - CRUD de categorias

--- app/Category.php
+++ app/Category.php
@@ -11,9 +11,10 @@ class Category extends Model
     public function products() // rel N:N // procura tabela pivot pela ordem alfabética dops models category_product
     {
         return $this->belongsToMany(Product::class);
-        // return $this->belongsToMany(Product::class, 'products_categories'); /// nomeclatura fora de padrão para encontar a tabela pivot
     }
 }
+
+// return $this->belongsToMany(Product::class, 'products_categories'); /// nomeclatura fora de padrão para encontar a tabela pivot
 
 // 1:1 - Um pra um (Usuário e loja) hasOne e belongsTo
 // 1:N - Um pra Muitos (Loja e Produtos) hasMany e belongsTo
