3a3752fa08d773ac58885500a275521567ad0d8d
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-24 14:08:05 -0300

    110 - Concluindo adição ao carrinho

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,10 +6,26 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        dd(session()->get('cart'));
+    }
+
     public function add(Request $request)
     {
         $product = $request->get('product');
 
-        dd($product);
+        if (session()->has('cart')) {
+
+            session()->push('cart', $product);
+
+        } else {
+            $products[] = $product;
+
+            session()->put('cart', $product);
+        }
+
+        flash('Produto adicionado ao carrinho!')->success();
+        return redirect()->route('product.single', ['slug' => $product['slug']]);
     }
 }
