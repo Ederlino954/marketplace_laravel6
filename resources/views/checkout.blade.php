4b93f381d4f292c228d190a304ababe26531c6c9
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-25 18:26:53 -0300

    126 - Exibindo opções de parcelamento

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -35,6 +35,8 @@
                             <label>Código de Segurança</label>
                             <input type="text" class="form-control" name="card_cvv">
                         </div>
+
+                        <div class="col-md-12 installments form-group"></div>
                     </div>
 
                     <button class="btn-success btn-lg">Efetuar Pagamento</button>
@@ -82,16 +84,32 @@
                     brand: brand,
                     maxInstallmentNoInterest: 0,
                     success: function (res) {
-                        console.log(res);
+                        let selectInstallments = drawSelectInstallments(res.installments[brand]);
+                        document.querySelector('div.installments').innerHTML = selectInstallments;
                     },
                     error: function (err) {
-
+                        console.log(err);
                     },
                     complete: function (res) {
 
                     }
 
                 })
+            }
+
+            function drawSelectInstallments(installments) {
+                let select = '<label>Opções de Parcelamento:</label>';
+
+                select += '<select class="form-control">';
+
+                for(let l of installments) {
+                    select += `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x de ${l.installmentAmount} - Total fica ${l.totalAmount}</option>`;
+                }
+
+
+                select += '</select>';
+
+                return select;
             }
 
         </script>
