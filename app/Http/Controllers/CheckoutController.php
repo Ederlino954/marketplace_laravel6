94159282e3bad9fb37b8f0b6d9d96ba35cfee041
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-25 13:29:36 -0300

    122 - Criando Sessão do pagseguro

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -11,7 +11,22 @@ class CheckoutController extends Controller
         if(!auth()->check()) {
             return redirect()->route('login');
         }
+        $this->makePagseguroSession();
+
+        var_dump(session()->get('pagseguro_session_code'));
 
         return view('checkout');
+    }
+
+    private function makePagseguroSession()
+    {
+        if(!session()->has('pagseguro_session_code')) {
+
+            $sessionCode = \PagSeguro\Services\Session::create(
+                \PagSeguro\Configuration\Configure::getAccountCredentials()
+            );
+
+            session()->put('pagseguro_session_code', $sessionCode->getResult());
+        }
     }
 }
