418f34a3fb5a5d1aea6426dbc18d50699ba71584
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-29 19:49:47 -0300

    169 - Melhoria na tela de notificação da loja

--- routes/web.php
+++ routes/web.php
@@ -195,6 +195,7 @@ Route::group(['middleware' => ['auth']], function () {
 
         Route::get('notifications', 'NotificationController@notifications')->name('notifications.index');
         Route::get('notifications/read-all', 'NotificationController@readAll')->name('notifications.read.all');
+        Route::get('notifications/read/{notification}', 'NotificationController@read')->name('notifications.read');
 
         // Route::prefix('stores')->name('stores.')->group(function(){
 
