50adce25a3c780abfc5dafaf0b61a17d54ab4664
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-29 20:14:45 -0300

    170 - Notificação via e-mail

--- app/Notifications/StoreReceiveNewOrder.php
+++ app/Notifications/StoreReceiveNewOrder.php
@@ -30,7 +30,7 @@ class StoreReceiveNewOrder extends Notification
     public function via($notifiable)
     {
         // return ['mail'];
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -42,9 +42,11 @@ class StoreReceiveNewOrder extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->subject('Você recebeu um novo pedido!')
+                    ->greeting('Olá vendedor, tudo bem?')
+                    ->line('Você recebeu um novo pedido na loja!')
+                    ->action('ver pedido', route('admin.orders.my'));
+
     }
 
     /**
