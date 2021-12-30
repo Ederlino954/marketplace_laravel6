51ee48d43a625dec30603d5991e3713fda95fbda
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2021-12-30 13:48:10 -0300

    178 - Máscara de telefones para lojas

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,8 @@ try {
 
     require('bootstrap');
 } catch (e) {}
+// -------acrescentado
+var Inputmask = require('inputmask');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
